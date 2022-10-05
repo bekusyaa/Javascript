873fb72abbec8d4ad47dcf9d80c8f3deaf38c731
Bekzhan Nurbekov <nurbekovbekzan9@gmail.com>
2022-10-05 15:00:24 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console. log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+ 
+console.log(obj);
+
+alert(5);
+[].push('a');
+
+
+
+const vehicleBodyWidth = 5000;
+const vehicleBodyLenght = 4000;
+
+console.log("width of car body: " + vehicleBodyWidth + ', lenght' + vehicleBodyLenght);
+
+
+// Request 
+// data 
+// response
+
+
+// snake_case 
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+let COLOR_RED = 'F00';
+
