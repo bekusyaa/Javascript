7b7978599fe234ff1a551c1fdfe9410b4bf8e28c
bekusyaa <110021512+bekusyaa@users.noreply.github.com>
2022-10-10 09:41:51 +0600

    operator in js

--- js/script.js
+++ js/script.js
@@ -116,3 +116,30 @@ console.log(`https://someurl.com/${category}/5`);
 const user ="Ivan";
 alert(`Hello, ${user}$.`);
 
+console.log('arr'+" - object");
+console.log(4 + 5);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2*4 == 8);
+
+
+const isChecked = true,
+      isClose = true,
+      notClosed = false;
+
+console.log(isChecked && isClose && !notClosed);
+
+const itChecked = true,
+      itClose = false;
+
+console.log(itChecked || itClose);
+
