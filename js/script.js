a850b03bc5de256007b9abd39f2b94d2d1cd4856
bekusyaa <110021512+bekusyaa@users.noreply.github.com>
2022-10-18 12:10:17 +0600

    cycles and conditions

--- js/script.js
+++ js/script.js
@@ -93,8 +93,8 @@ console.log(arrObj[1]);
 
 alert('Hello'); 
 
-const result = confirm("are you here?");
-console.log(result);
+const results = confirm("are you here?");
+console.log(results);
 
 const answer = prompt("are you 18", "18");
 console.log(answer);
@@ -198,3 +198,69 @@ while (num < 55);
 for (let i = 1; i < 8; i++) { 
     console.log(i);
 }
+
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+let result = '';
+const lenght = 7;
+
+for (let i = 1; i < lenght; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '/n';
+}
+
+
+console.log(result);
+
+
+// for (let i = 5; i < 11; i++) {
+//     console.log(i);
+// }
+
+
+
+// for (let i = 20; i > 9; i--) {
+//     console.log(i);
+//     if (i === 13) {
+//         break;
+//     }
+// }
+
+
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+    for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+let i = 2;
+
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++;
+}
+
