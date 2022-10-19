db9252961f0ac23d6782446cf1797b0b82d80bac
bekusyaa <110021512+bekusyaa@users.noreply.github.com>
2022-10-19 13:43:39 +0600

    return undone

--- js/script.js
+++ js/script.js
@@ -1,279 +1,279 @@
 "use strict";
 
-// let number = 5;
-// const leftBorderWidth = 1;
+let number = 5;
+const leftBorderWidth = 1;
 
-// number = 10;
-// console. log(number);
+number = 10;
+console. log(number);
 
-// const obj = {
-//     a: 50
-// };
+const obj = {
+    a: 50
+};
 
-// obj.a = 10;
+obj.a = 10;
  
-// console.log(obj);
+console.log(obj);
 
-// alert(5);
-// [].push('a');
-
-
-
-// const vehicleBodyWidth = 5000;
-// const vehicleBodyLenght = 4000;
-
-// console.log("width of car body: " + vehicleBodyWidth + ', lenght' + vehicleBodyLenght);
-
-
-// // Request 
-// // data 
-// // response
-
-
-// // snake_case 
-// // UPPER_SNAKE_CASE
-// // Kebab-case
-// // PascalCase
-
-// let COLOR_RED = 'F00';
-
-
-// let equation = 4.6;
-
-// console.log(-4/0);
-// console.log('string' * 4);
-
-// const person = "string"; // for strings 
-
-// const bool = true;
-// const bool1 = false; // for boolen types of data
-
-// const obj2 = {
-//     name: "John",
-//     age: 25, 
-//     isMarried: false
-// };
-
-// console.log(obj2.name);
-// // it's an object it uses {} to put smt in. and to show something you need write
-// //console.log(name of the variable the (...) then what you wanna show);
-
-
-// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+alert(5);
+[].push('a');
 
 
 
+const vehicleBodyWidth = 5000;
+const vehicleBodyLenght = 4000;
 
-// console.log(arr[0]);
-// console.log(arr[1]);
-// console.log(arr[2]);
-
-
-// const arrs = [1, 2, 3];
-// const arrObj = {
-//     a: 1, 
-//     1: 2,
-//     c: 3
-// };
-
-// arrObj.b = '1234';
-
-// console.log(arrObj.b);
-
-// console.log(arrs[0]);
-// console.log(arrs[1]);
-// console.log(arrObj.a);
-// console.log(arrObj[1]);
+console.log("width of car body: " + vehicleBodyWidth + ', lenght' + vehicleBodyLenght);
 
 
-
-// // const obj3 = {a: 1, b: 2};
-// // console.log(obj3.a);
-
-
-// alert('Hello'); 
-
-// const results = confirm("are you here?");
-// console.log(results);
-
-// const answer = prompt("are you 18", "18");
-// console.log(answer);
+// Request 
+// data 
+// response
 
 
-//  const answers = [];
+// snake_case 
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
 
-//  answers[0] = prompt('What are name?', '');
-//  answers[1] = prompt('What is your last name?', '');
-//  answers[2] = prompt('How old are you?', '');
-
-
-
-// const category = 'toys';
-
-// console.log(`https://someurl.com/${category}/5`);
-
-// const user ="Ivan";
-// alert(`Hello, ${user}$.`);
-
-// console.log('arr'+" - object");
-// console.log(4 + 5);
-
-// let incr = 10,
-//     decr = 10;
-
-// // ++incr;
-// // --decr;
-
-// console.log(++incr);
-// console.log(--decr);
-
-// console.log(5%2);
-// console.log(2*4 == 8);
+let COLOR_RED = 'F00';
 
 
-// const isChecked = true,
-//       isClose = true,
-//       notClosed = false;
+let equation = 4.6;
 
-// console.log(isChecked && isClose && !notClosed);
+console.log(-4/0);
+console.log('string' * 4);
 
-// const itChecked = true,
-//       itClose = false;
+const person = "string"; // for strings 
 
-// console.log(itChecked || itClose);
+const bool = true;
+const bool1 = false; // for boolen types of data
+
+const obj2 = {
+    name: "John",
+    age: 25, 
+    isMarried: false
+};
+
+console.log(obj2.name);
+// it's an object it uses {} to put smt in. and to show something you need write
+//console.log(name of the variable the (...) then what you wanna show);
 
 
-//  if (4 == 9) {
-//     console.log('Ok!');
-//  } else {
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+
+
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
+
+const arrs = [1, 2, 3];
+const arrObj = {
+    a: 1, 
+    1: 2,
+    c: 3
+};
+
+arrObj.b = '1234';
+
+console.log(arrObj.b);
+
+console.log(arrs[0]);
+console.log(arrs[1]);
+console.log(arrObj.a);
+console.log(arrObj[1]);
+
+
+
+// const obj3 = {a: 1, b: 2};
+// console.log(obj3.a);
+
+
+alert('Hello'); 
+
+const results = confirm("are you here?");
+console.log(results);
+
+const answer = prompt("are you 18", "18");
+console.log(answer);
+
+
+ const answers = [];
+
+ answers[0] = prompt('What are name?', '');
+ answers[1] = prompt('What is your last name?', '');
+ answers[2] = prompt('How old are you?', '');
+
+
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user ="Ivan";
+alert(`Hello, ${user}$.`);
+
+console.log('arr'+" - object");
+console.log(4 + 5);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2*4 == 8);
+
+
+const isChecked = true,
+      isClose = true,
+      notClosed = false;
+
+console.log(isChecked && isClose && !notClosed);
+
+const itChecked = true,
+      itClose = false;
+
+console.log(itChecked || itClose);
+
+
+ if (4 == 9) {
+    console.log('Ok!');
+ } else {
+    console.log('Error');
+ }
+
+ if (4 == 4) {
+    console.log('Ok!');
+ } else {
+    console.log('Error');
+ }
+
+// const num = 50;
+
+// if (num < 49) {
 //     console.log('Error');
-//  }
-
-//  if (4 == 4) {
+// } else if (num > 100) {
+//     console.log('A lot');
+// } else {
 //     console.log('Ok!');
-//  } else {
-//     console.log('Error');
-//  }
-
-// // const num = 50;
-
-// // if (num < 49) {
-// //     console.log('Error');
-// // } else if (num > 100) {
-// //     console.log('A lot');
-// // } else {
-// //     console.log('Ok!');
-// // }
-
-
-// // switch (numm) {
-// //     case 49:
-// //         console.log("incorrect");
-// //         break;
-// //     case 100:
-// //         console.log("incorrect");
-// //         break;
-// //     case 50:
-// //         console.log("correct");
-// //         break;
-// //     default:
-// //         console.log("Not in this time");
-// //         break;
-// // }
-
-
-// let num = 50;
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
 // }
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
 
-// for (let i = 1; i < 8; i++) { 
+// switch (numm) {
+//     case 49:
+//         console.log("incorrect");
+//         break;
+//     case 100:
+//         console.log("incorrect");
+//         break;
+//     case 50:
+//         console.log("correct");
+//         break;
+//     default:
+//         console.log("Not in this time");
+//         break;
+// }
+
+
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) { 
+    console.log(i);
+}
+
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+let result = '';
+const lenght = 7;
+
+for (let i = 1; i < lenght; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '/n';
+}
+
+
+console.log(result);
+
+
+// for (let i = 5; i < 11; i++) {
 //     console.log(i);
 // }
 
 
-// for (let i = 0; i < 3; i++) {
+
+// for (let i = 20; i > 9; i--) {
 //     console.log(i);
-//     for (let j = 0; j < 3; j++) {
-//         console.log(j);
-//     }
-// }
-
-// let result = '';
-// const lenght = 7;
-
-// for (let i = 1; i < lenght; i++) {
-
-//     for (let j = 0; j < i; j++) {
-//         result += "*";
-//     }
-
-//     result += '/n';
-// }
-
-
-// console.log(result);
-
-
-// // for (let i = 5; i < 11; i++) {
-// //     console.log(i);
-// // }
-
-
-
-// // for (let i = 20; i > 9; i--) {
-// //     console.log(i);
-// //     if (i === 13) {
-// //         break;
-// //     }
-// // }
-
-
-
-// for (let i = 2; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
+//     if (i === 13) {
+//         break;
 //     }
 // }
 
 
-//     for (let i = 2; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
-//     }
-// }
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
 
 
-// let i = 2;
-
-// while (i <= 16) {
-//     if (i % 2 === 0) {
-//         i++;
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-//     i++;
-// }
+    for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
 
 
-let num = 20;
+let i = 2;
+
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++;
+}
+
+
+let num1 = 20;
 
 function showFirstMessage (text) {
     console.log(text);
-    console.log(num);
+    console.log(num1);
 }
 
 showFirstMessage("Hello World");
-console.log(num);
+console.log(num1);
 
 // function calc(a, b) {
 //     return (a + b);
@@ -316,3 +316,16 @@ function convert (amout, curr) {
 
 convert(500 , usdCurr);
 convert(500 , eurCurr);
+
+const usdCurr1 = 28;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    console.log(curr * amount);
+}
+
+function promotion(result) {
+    console.log(result * discount);
+}
+
+convert(500, usdCurr1); 
