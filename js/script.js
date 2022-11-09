9d0404e7303a7307b23ffe1d87fded239619cb35
bekusyaa <110021512+bekusyaa@users.noreply.github.com>
2022-11-09 09:50:40 +0600

    arrays and pseudo-arrays

--- js/script.js
+++ js/script.js
@@ -381,3 +381,35 @@
 // console.log(counter);
 
 
+// const arr = [2, 23, 36, 8, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum (a, b) {
+//     return a - b;
+// }
+
+
+// arr[99] = 0;
+// console.log(arr.length);
+
+// arr.forEach(function(item, i, arr){
+//     console.log(`${i}: ${item}: внутри массива ${arr}`);
+// });
+ 
+
+// arr.pop();
+// arr.push(10);
+
+// for (let i = 0; i < arr.length; i++) {
+//      console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; ')); 
