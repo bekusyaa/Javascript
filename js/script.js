6ee184d8d3bc89d4884dc808aa928e2b628dce92
bekusyaa <110021512+bekusyaa@users.noreply.github.com>
2022-11-10 11:33:44 +0600

    OOP

--- js/script.js
+++ js/script.js
@@ -413,3 +413,36 @@
 // const products = str.split(", ");
 // products.sort();
 // console.log(products.join('; ')); 
+
+let str = "some";
+let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1,2,3]);
+
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function () {
+        console.log("Hello!");
+    }
+};
+
+const jonh = Object.create(soldier);
+
+jonh.sayHello();
+
+
+// const jonh = {
+//     health: 100
+// };
+
+// jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier);
+
+
+// jonh.sayHello();
