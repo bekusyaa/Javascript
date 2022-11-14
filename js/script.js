ddc7c6fa774a87b26423f84eac4db136ec8f3251
bekusyaa <110021512+bekusyaa@users.noreply.github.com>
2022-11-14 07:58:15 +0600

    counter

--- js/script.js
+++ js/script.js
@@ -503,3 +503,43 @@
 
 
 
+// let number = 5;debugger
+
+// function logNumber() {
+//     console.log(number);debugger
+// }
+
+// number = 6;
+
+// logNumber();debugger
+
+// number = 8;
+
+// logNumber();debugger
+
+
+// function createCounter() {
+//     let counter = 0;
+
+//     const myFunction = function() {
+//         counter = counter + 1;
+//         return counter;
+//     };
+    
+
+//     return myFunction;
+// }
+
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+// const c4 = increment();
+// const c5 = increment();
+// const c6 = increment();
+// const c7 = increment();
+
+
+// console.log(c1, c2, c3, c4, c5, c6, c7);
+
+
